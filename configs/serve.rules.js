1e24361df2ab5892ce9e6095700d5aef4d15b4b6
hegdeashwin <ashwin.hegde3@gmail.com>
2015-08-29 16:32:20 +0530

    re-factors some gulp tasks

--- configs/serve.rules.js
+++ configs/serve.rules.js
@@ -4,13 +4,24 @@ module.exports = {
 	"lint": {
 		"rules": {
 			"html": {
-				"doctype-first": true,
-				"tag-pair": true,
-				"tag-self-close": true,
-				"id-unique": true,
-				"attr-lowercase": true,
-				"doctype-html5": true,
-				"head-script-disabled": true
+				"gateway": {
+					"doctype-first": true,
+					"tag-pair": true,
+					"tag-self-close": true,
+					"id-unique": true,
+					"attr-lowercase": true,
+					"doctype-html5": true,
+					"head-script-disabled": true
+				},
+				"templates": {
+					"doctype-first": false,
+					"tag-pair": true,
+					"tag-self-close": true,
+					"id-unique": true,
+					"attr-lowercase": true,
+					"doctype-html5": false,
+					"head-script-disabled": true
+				}
 			},
 			"css": {},
 			"js": {
