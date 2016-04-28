4fe2e22797abcf84cfd0ac521167215f1f355b2c
hegdeashwin <ashwin.hegde3@gmail.com>
2016-04-28 23:57:45 +0530

    Updates MyRepositories view

---
+++ src/views/MyRepositories/RepositoriesList.jsx
@@ -0,0 +1,76 @@
+'use strict';
+
+import React from 'react';
+
+require('./RepositoriesList.less');
+
+class RepositoriesList extends React.Component {
+  render() {
+    return (
+      <div className="my-repositories-list">
+        <div className="media list">
+          <div className="media-left remote-repo-logo text-center">
+            <i className="fa fa-github fa-3x"></i>
+          </div>
+          <div className="media-body">
+            <div className="col-md-6">
+              <h4 className="media-heading">
+                <a href="#">Protocore</a>
+                <a target="_blank" href="//github.com/hegdeashwin/Protocore" title="Visit: github.com/hegdeashwin/Protocore"></a>
+              </h4>
+              <p>Github Repository: <a target="_blank" href="//github.com/hegdeashwin/Protocore">github.com/hegdeashwin/Protocore</a></p>
+            </div>
+            <div className="col-md-6">
+              <p className="pull-right">
+                <a href="#" className="btn btn-sm btn-success">Open</a>
+                <a href="#" className="btn btn-sm btn-danger">Delete</a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="media list">
+          <div className="media-left remote-repo-logo text-center">
+            <i className="fa fa-bitbucket fa-3x"></i>
+          </div>
+          <div className="media-body">
+            <div className="col-md-6">
+              <h4 className="media-heading">
+                <a href="#">Protocore</a>
+                <a target="_blank" href="//github.com/hegdeashwin/Protocore" title="Visit: github.com/hegdeashwin/Protocore"></a>
+              </h4>
+              <p>Github Repository: <a target="_blank" href="//github.com/hegdeashwin/Protocore">github.com/hegdeashwin/Protocore</a></p>
+            </div>
+            <div className="col-md-6">
+              <p className="pull-right">
+                <a href="#" className="btn btn-sm btn-success">Open</a>
+                <a href="#" className="btn btn-sm btn-danger">Delete</a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="media list">
+          <div className="media-left remote-repo-logo text-center">
+            <i className="fa fa-gitlab fa-3x"></i>
+          </div>
+          <div className="media-body">
+            <div className="col-md-6">
+              <h4 className="media-heading">
+                <a href="#">Protocore</a>
+                <a target="_blank" href="//github.com/hegdeashwin/Protocore" title="Visit: github.com/hegdeashwin/Protocore"></a>
+              </h4>
+              <p>Github Repository: <a target="_blank" href="//github.com/hegdeashwin/Protocore">github.com/hegdeashwin/Protocore</a></p>
+            </div>
+            <div className="col-md-6">
+              <p className="pull-right">
+                <a href="#" className="btn btn-sm btn-success">Open</a>
+                <a href="#" className="btn btn-sm btn-danger">Delete</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default RepositoriesList;
