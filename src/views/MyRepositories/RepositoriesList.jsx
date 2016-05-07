99997f5cf6b492fcc35ecd67e83e6dcafaa1c784
hegdeashwin <ashwin.hegde3@gmail.com>
2016-05-07 20:21:45 +0530

    Re-factors repositorylist view

--- src/views/MyRepositories/RepositoriesList.jsx
+++ src/views/MyRepositories/RepositoriesList.jsx
@@ -9,7 +9,7 @@ class RepositoriesList extends React.Component {
   render() {
     return (
       <div className="my-repositories-list">
-        <div className="col-md-6 profile">
+        <div className="col-md-4 profile">
           <div className="profile-blog blog-border">
             <div className="media">
               <div className="media-left">
@@ -21,14 +21,21 @@ class RepositoriesList extends React.Component {
               </div>
             </div>
             <hr />
-            <ul className="list-inline share-list">
-              <li><Link to="repository/:repository/dashboard" className="btn btn-success btn-sm">Open</Link></li>
-              <li><Link to="repository/:repository/dashboard" className="btn btn-danger btn-sm">Delete</Link></li>
-            </ul>
+            <form className="form-inline">
+              <div className="checkbox">
+                <label>
+                  <input type="checkbox" /> <span className="">Open in new browser tab.</span>
+                </label>
+              </div>
+              <ul className="list-inline share-list pull-right">
+                <li><Link to="repository/:repository/dashboard" className="btn btn-success btn-sm">Open</Link></li>
+                <li><Link to="repository/:repository/dashboard" className="btn btn-danger btn-sm">Delete</Link></li>
+              </ul>
+            </form>
           </div>
         </div>
 
-        <div className="col-md-6 profile">
+        <div className="col-md-4 profile">
           <div className="profile-blog blog-border">
             <div className="media">
               <div className="media-left">
@@ -40,14 +47,21 @@ class RepositoriesList extends React.Component {
               </div>
             </div>
             <hr />
-            <ul className="list-inline share-list">
-              <li><Link to="repository/:repository/dashboard" className="btn btn-success btn-sm">Open</Link></li>
-              <li><Link to="repository/:repository/dashboard" className="btn btn-danger btn-sm">Delete</Link></li>
-            </ul>
+            <form className="form-inline">
+              <div className="checkbox">
+                <label>
+                  <input type="checkbox" /> <span className="">Open in new browser tab.</span>
+                </label>
+              </div>
+              <ul className="list-inline share-list pull-right">
+                <li><Link to="repository/:repository/dashboard" className="btn btn-success btn-sm">Open</Link></li>
+                <li><Link to="repository/:repository/dashboard" className="btn btn-danger btn-sm">Delete</Link></li>
+              </ul>
+            </form>
           </div>
         </div>
 
-        <div className="col-md-6 profile">
+        <div className="col-md-4 profile">
           <div className="profile-blog blog-border">
             <div className="media">
               <div className="media-left">
@@ -59,10 +73,17 @@ class RepositoriesList extends React.Component {
               </div>
             </div>
             <hr />
-            <ul className="list-inline share-list">
-              <li><Link to="repository/:repository/dashboard" className="btn btn-success btn-sm">Open</Link></li>
-              <li><Link to="repository/:repository/dashboard" className="btn btn-danger btn-sm">Delete</Link></li>
-            </ul>
+            <form className="form-inline">
+              <div className="checkbox">
+                <label>
+                  <input type="checkbox" /> <span className="">Open in new browser tab.</span>
+                </label>
+              </div>
+              <ul className="list-inline share-list pull-right">
+                <li><Link to="repository/:repository/dashboard" className="btn btn-success btn-sm">Open</Link></li>
+                <li><Link to="repository/:repository/dashboard" className="btn btn-danger btn-sm">Delete</Link></li>
+              </ul>
+            </form>
           </div>
         </div>
 
