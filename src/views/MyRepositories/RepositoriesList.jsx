cf4742ed7aa8c06ecc52e0a20661b1a4b942c1b7
hegdeashwin <ashwin.hegde3@gmail.com>
2016-05-08 17:46:09 +0530

    Re-factors views

--- src/views/MyRepositories/RepositoriesList.jsx
+++ src/views/MyRepositories/RepositoriesList.jsx
@@ -13,24 +13,20 @@ class RepositoriesList extends React.Component {
           <div className="profile-blog blog-border">
             <div className="media">
               <div className="media-left">
-                <i className="fa fa-github fa-4x"></i>
+                <i className="fa fa-bitbucket fa-4x"></i>
               </div>
               <div className="media-body">
-                <h4 className="media-heading lead">Protocore</h4>
+                <h4 className="media-heading lead repository-name">Protocore</h4>
                 <span className="label label-primary">Bare Repository</span>
                 <p>
-                  <span className="label label-primary">Location: C:\Projects\Protocore</span>
+                  <strong>Location:</strong>
+                  <span className="location">C:\Projects\Protocore</span>
                 </p>
               </div>
             </div>
             <hr />
             <form className="form-inline">
-              <div className="checkbox">
-                <label>
-                  <input type="checkbox" /> <span className="">Open in new browser tab.</span>
-                </label>
-              </div>
-              <ul className="list-inline share-list pull-right">
+              <ul className="list-inline share-list">
                 <li><Link to="repository/:repository/dashboard" className="btn btn-success btn-sm">Open</Link></li>
                 <li><Link to="repository/:repository/dashboard" className="btn btn-danger btn-sm">Delete</Link></li>
               </ul>
@@ -45,21 +41,17 @@ class RepositoriesList extends React.Component {
                 <i className="fa fa-bitbucket fa-4x"></i>
               </div>
               <div className="media-body">
-                <h4 className="media-heading lead">Protocore</h4>
+                <h4 className="media-heading lead repository-name">Protocore</h4>
                 <span className="label label-primary">Bare Repository</span>
                 <p>
-                  <span className="label label-primary">Location: C:\Projects\Protocore</span>
+                  <strong>Location:</strong>
+                  <span className="location">C:\Projects\Protocore</span>
                 </p>
               </div>
             </div>
             <hr />
             <form className="form-inline">
-              <div className="checkbox">
-                <label>
-                  <input type="checkbox" /> <span className="">Open in new browser tab.</span>
-                </label>
-              </div>
-              <ul className="list-inline share-list pull-right">
+              <ul className="list-inline share-list">
                 <li><Link to="repository/:repository/dashboard" className="btn btn-success btn-sm">Open</Link></li>
                 <li><Link to="repository/:repository/dashboard" className="btn btn-danger btn-sm">Delete</Link></li>
               </ul>
@@ -71,24 +63,20 @@ class RepositoriesList extends React.Component {
           <div className="profile-blog blog-border">
             <div className="media">
               <div className="media-left">
-                <i className="fa fa-gitlab fa-4x"></i>
+                <i className="fa fa-bitbucket fa-4x"></i>
               </div>
               <div className="media-body">
-                <h4 className="media-heading lead">Protocore</h4>
+                <h4 className="media-heading lead repository-name">Protocore</h4>
                 <span className="label label-primary">Bare Repository</span>
                 <p>
-                  <span className="label label-primary">Location: C:\Projects\Protocore</span>
+                  <strong>Location:</strong>
+                  <span className="location">C:\Projects\Protocore</span>
                 </p>
               </div>
             </div>
             <hr />
             <form className="form-inline">
-              <div className="checkbox">
-                <label>
-                  <input type="checkbox" /> <span className="">Open in new browser tab.</span>
-                </label>
-              </div>
-              <ul className="list-inline share-list pull-right">
+              <ul className="list-inline share-list">
                 <li><Link to="repository/:repository/dashboard" className="btn btn-success btn-sm">Open</Link></li>
                 <li><Link to="repository/:repository/dashboard" className="btn btn-danger btn-sm">Delete</Link></li>
               </ul>
