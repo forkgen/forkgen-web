a19f7f5ac6327e6a36b38e6483f261b36244f0f4
hegdeashwin <ashwin.hegde3@gmail.com>
2016-06-04 13:49:34 +0530

    Re-factors Panel layout

--- src/views/Setup/Clone.jsx
+++ src/views/Setup/Clone.jsx
@@ -2,29 +2,32 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Panel } from 'react-bootstrap';
 
 class Clone extends React.Component {
   render() {
     return (
-      <div className="media project-container">
-        <div className="media-body">
-          <form>
-            <div className="form-group">
-              <label for="createRepository">Specify Github / Gitlab / BitBucket remote repository URL:</label>
-              <input type="text" className="form-control" placeholder="E.g. https://github.com/hegdeashwin/forkgen.git" />
-            </div>
-            <div className="form-group">
-              <label for="createRepository">Specify repository location:</label>
-              <div className="input-group">
-                <input type="text" className="form-control" placeholder="E.g. C:\Workspace" />
-                <span className="input-group-btn">
-                  <button className="btn btn-success" type="button" title="Clone from Github remote repository">Clone</button>
-                </span>
+      <Panel header="Clone an existing repository">
+        <div className="media project-container">
+          <div className="media-body">
+            <form>
+              <div className="form-group">
+                <label for="createRepository">Specify Github / Gitlab / BitBucket remote repository URL:</label>
+                <input type="text" className="form-control" placeholder="E.g. https://github.com/hegdeashwin/forkgen.git" />
               </div>
-            </div>
-          </form>
+              <div className="form-group">
+                <label for="createRepository">Specify repository location:</label>
+                <div className="input-group">
+                  <input type="text" className="form-control" placeholder="E.g. C:\Workspace" />
+                  <span className="input-group-btn">
+                    <button className="btn btn-success" type="button" title="Clone from Github remote repository">Clone</button>
+                  </span>
+                </div>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
+      </Panel>
     );
   }
 }
--- src/views/Setup/OpenExist.jsx
+++ src/views/Setup/OpenExist.jsx
@@ -2,25 +2,28 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Panel } from 'react-bootstrap';
 
 class OpenExist extends React.Component {
   render() {
     return (
-      <div className="media project-container">
-        <div className="media-body">
-          <form>
-            <div className="form-group">
-              <label for="createRepository">Specify repository location:</label>
-              <div className="input-group">
-                <input type="text" className="form-control" placeholder="E.g. C:\Workspace" />
-                <span className="input-group-btn">
-                  <button className="btn btn-success" type="button" title="Open existing respository">Open</button>
-                </span>
+      <Panel header="Open an existing respository">
+        <div className="media project-container">
+          <div className="media-body">
+            <form>
+              <div className="form-group">
+                <label for="createRepository">Specify repository location:</label>
+                <div className="input-group">
+                  <input type="text" className="form-control" placeholder="E.g. C:\Workspace" />
+                  <span className="input-group-btn">
+                    <button className="btn btn-success" type="button" title="Open existing respository">Open</button>
+                  </span>
+                </div>
               </div>
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
-      </div>
+      </Panel>
     );
   }
 }
