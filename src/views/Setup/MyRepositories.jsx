05cffc8bf12b4a582bb61193548a63c14a8f10e4
hegdeashwin <ashwin.hegde3@gmail.com>
2016-06-06 00:10:23 +0530

    Adds repository icon

--- src/views/Setup/MyRepositories.jsx
+++ src/views/Setup/MyRepositories.jsx
@@ -17,44 +17,48 @@ class Clone extends React.Component {
           <div className="form-group my-repository-list">
             <div className="list-group">
               <Link to="protocore/open" className="list-group-item">
-                <h4 className="list-group-item-heading">Protocore</h4>
-                <p className="list-group-item-text">C:\Projects\Protocore</p>
+                <div className="media">
+                  <div className="media-left">
+                    <i className="fa fa-3x fa-github"></i>
+                  </div>
+                  <div className="media-body">
+                    <h4 className="media-heading">Protocore</h4>
+                    <p className="list-group-item-text">C:\Projects\Protocore</p>
+                  </div>
+                </div>
               </Link>
               <Link to="protocore/open" className="list-group-item">
-                <h4 className="list-group-item-heading">Protocore</h4>
-                <p className="list-group-item-text">C:\Projects\Protocore</p>
+                <div className="media">
+                  <div className="media-left">
+                    <i className="fa fa-3x fa-bitbucket"></i>
+                  </div>
+                  <div className="media-body">
+                    <h4 className="media-heading">Protocore</h4>
+                    <p className="list-group-item-text">C:\Projects\Protocore</p>
+                  </div>
+                </div>
               </Link>
               <Link to="protocore/open" className="list-group-item">
-                <h4 className="list-group-item-heading">Protocore</h4>
-                <p className="list-group-item-text">C:\Projects\Protocore</p>
+                <div className="media">
+                  <div className="media-left">
+                    <i className="fa fa-3x fa-gitlab"></i>
+                  </div>
+                  <div className="media-body">
+                    <h4 className="media-heading">Protocore</h4>
+                    <p className="list-group-item-text">C:\Projects\Protocore</p>
+                  </div>
+                </div>
               </Link>
               <Link to="protocore/open" className="list-group-item">
-                <h4 className="list-group-item-heading">Protocore</h4>
-                <p className="list-group-item-text">C:\Projects\Protocore</p>
-              </Link>
-              <Link to="protocore/open" className="list-group-item">
-                <h4 className="list-group-item-heading">Protocore</h4>
-                <p className="list-group-item-text">C:\Projects\Protocore</p>
-              </Link>
-              <Link to="protocore/open" className="list-group-item">
-                <h4 className="list-group-item-heading">Protocore</h4>
-                <p className="list-group-item-text">C:\Projects\Protocore</p>
-              </Link>
-              <Link to="protocore/open" className="list-group-item">
-                <h4 className="list-group-item-heading">Protocore</h4>
-                <p className="list-group-item-text">C:\Projects\Protocore</p>
-              </Link>
-              <Link to="protocore/open" className="list-group-item">
-                <h4 className="list-group-item-heading">Protocore</h4>
-                <p className="list-group-item-text">C:\Projects\Protocore</p>
-              </Link>
-              <Link to="protocore/open" className="list-group-item">
-                <h4 className="list-group-item-heading">Protocore</h4>
-                <p className="list-group-item-text">C:\Projects\Protocore</p>
-              </Link>
-              <Link to="protocore/open" className="list-group-item">
-                <h4 className="list-group-item-heading">Protocore</h4>
-                <p className="list-group-item-text">C:\Projects\Protocore</p>
+                <div className="media">
+                  <div className="media-left">
+                    <i className="fa fa-3x fa-git"></i>
+                  </div>
+                  <div className="media-body">
+                    <h4 className="media-heading">Protocore</h4>
+                    <p className="list-group-item-text">C:\Projects\Protocore</p>
+                  </div>
+                </div>
               </Link>
             </div>
           </div>
