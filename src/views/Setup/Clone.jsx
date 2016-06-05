768ffdf2a504b0c1bb656bb34246448f18f13f32
hegdeashwin <ashwin.hegde3@gmail.com>
2016-06-05 23:07:13 +0530

    Re-factor clone component

--- src/views/Setup/Clone.jsx
+++ src/views/Setup/Clone.jsx
@@ -7,26 +7,22 @@ import { Panel } from 'react-bootstrap';
 class Clone extends React.Component {
   render() {
     return (
-      <Panel header="Clone an existing repository">
-        <div className="media project-container">
-          <div className="media-body">
-            <form>
-              <div className="form-group">
-                <label for="createRepository">Specify Github / Gitlab / BitBucket remote repository URL:</label>
-                <input type="text" className="form-control" placeholder="E.g. https://github.com/hegdeashwin/forkgen.git" />
-              </div>
-              <div className="form-group">
-                <label for="createRepository">Specify repository location:</label>
-                <div className="input-group">
-                  <input type="text" className="form-control" placeholder="E.g. C:\Workspace" />
-                  <span className="input-group-btn">
-                    <button className="btn btn-success" type="button" title="Clone from Github remote repository">Clone</button>
-                  </span>
-                </div>
-              </div>
-            </form>
+      <Panel header="Clone an existing repository" className="clone-component">
+        <form>
+          <div className="form-group">
+            <label for="createRepository">Specify Github / Gitlab / BitBucket remote repository URL:</label>
+            <input type="text" className="form-control" placeholder="E.g. https://github.com/hegdeashwin/forkgen.git" />
           </div>
-        </div>
+          <div className="form-group">
+            <label for="createRepository">Specify repository location:</label>
+            <div className="input-group">
+              <input type="text" className="form-control" placeholder="E.g. C:\Workspace" />
+              <span className="input-group-btn">
+                <button className="btn btn-success" type="button" title="Clone from Github remote repository">Clone</button>
+              </span>
+            </div>
+          </div>
+        </form>
       </Panel>
     );
   }
