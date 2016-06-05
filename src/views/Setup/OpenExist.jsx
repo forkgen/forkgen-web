ee53803bd67c1a2b13d21622fcad40b84b0e3580
hegdeashwin <ashwin.hegde3@gmail.com>
2016-06-05 23:06:51 +0530

    Re-factor openexist component

--- src/views/Setup/OpenExist.jsx
+++ src/views/Setup/OpenExist.jsx
@@ -7,22 +7,18 @@ import { Panel } from 'react-bootstrap';
 class OpenExist extends React.Component {
   render() {
     return (
-      <Panel header="Open an existing respository">
-        <div className="media project-container">
-          <div className="media-body">
-            <form>
-              <div className="form-group">
-                <label for="createRepository">Specify repository location:</label>
-                <div className="input-group">
-                  <input type="text" className="form-control" placeholder="E.g. C:\Workspace" />
-                  <span className="input-group-btn">
-                    <button className="btn btn-success" type="button" title="Open existing respository">Open</button>
-                  </span>
-                </div>
-              </div>
-            </form>
+      <Panel header="Open an existing respository" className="openexist-component">
+        <form>
+          <div className="form-group">
+            <label for="createRepository">Specify repository location:</label>
+            <div className="input-group">
+              <input type="text" className="form-control" placeholder="E.g. C:\Workspace" />
+              <span className="input-group-btn">
+                <button className="btn btn-success" type="button" title="Open existing respository">Open</button>
+              </span>
+            </div>
           </div>
-        </div>
+        </form>
       </Panel>
     );
   }
